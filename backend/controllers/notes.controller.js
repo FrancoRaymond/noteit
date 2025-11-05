c040f4b4192c3c447d4dccfc96cb4193c8410631
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-11-06 00:23:53 +0200

    setup Express server with initial routes and controllers

---
+++ backend/controllers/notes.controller.js
@@ -0,0 +1,15 @@
+export const getNotes = (req, res) => {
+    res.status(200).json({message: "you fetched notes"})
+}
+
+export const createNote = (req, res) => {
+    res.status(201).json({message: "note created successfully"})
+}
+
+export const updateNote = (req, res) => {
+    res.status(200).json({message: "note updated successfully"})
+}
+
+export const deleteNote = (req, res) => {
+    res.status(200).json({message: "note deleted successfully"})
+}
---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import notesRoutes from './routes/notes.routes.js'
+
+const app = express()
+
+app.use("/api/notes", notesRoutes)
+
+
+app.listen(5001, () => {
+    console.log("server started on PORT: 5001")
+})
