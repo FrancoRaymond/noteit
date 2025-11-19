10c0dcce4e35d6272e2277dec8a45e17429a4649
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-11-19 16:03:53 +0200

    added a default route and fixed noteCard styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Route path='/' element={ <Home /> } />
         <Route path='/create' element={ <CreateNote /> } />
         <Route path='/note/:id' element={ <NoteDetail /> } />
+        <Route path='*' element={<Home /> } />
       </Routes>
     </div>
   )
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -30,8 +30,8 @@ const NoteCard = ({note, setNotes}) => {
   return (
     <Link to={`/note/${note._id}`} className='bg-gray-900 rounded-md py-4 px-3 flex flex-col gap-2 border-t-4 border-amber-400 transition-all duration-200 hover:shadow-2xl active:scale-105'>
       <h2 className='font-semibold text-gray-100'>{note.title}</h2>
-      <p className='text-gray-300 text-sm'>{note.content}</p>
-      <div className='flex text-xs items-center gap-5 mt-5'>
+      <p className='text-gray-300 text-sm mb-3'>{note.content.slice(0, 125) + "..."}</p>
+      <div className='flex text-xs items-center gap-5 mt-auto'>
           <span className='grow text-gray-400'>{formatDate(note.createdAt)}</span>
           <button className='cursor-pointer'><img src={edit} alt="" className='size-4 hover:scale-110 transition duration-200'/></button>
           <button onClick={(e) => handleDelete(e, note._id)} className='cursor-pointer'><img src={del} alt="" className='size-4 hover:scale-110 transition duration-200'/></button>
@@ -40,4 +40,4 @@ const NoteCard = ({note, setNotes}) => {
   )
 }
 
-export default NoteCard
+export default NoteCard;
